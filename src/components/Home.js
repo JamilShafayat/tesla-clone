26fb38092f1c8f1e769b698afb65ecb0c39a6b3a
JamilShafayat <jamilshafayat8@gmail.com>
2021-11-15 18:35:01 +0600

    setup

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components'
+import Section from './Section';
+
+const Container = styled.div`
+    height: 100vh;
+`;
+
+function Home() {
+    return (
+        <Container>
+            <Section />
+            <Section />
+            <Section />
+            <Section />
+        </Container>
+    )
+}
+
+export default Home
